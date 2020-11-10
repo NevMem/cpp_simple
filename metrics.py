ae2f250be49d378a579fd4b18c00dcdcac88e29b
NevMem <memlolkek@gmail.com>
2020-11-10 23:26:44 +0300

    Rename + some metrics

--- metrics.py
+++ metrics.py
@@ -1,7 +1,10 @@
 from cmake_generator import CMakeGenerator
 from typing import List
+import json
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import shutil
 import subprocess as sp
 import sys
 import time
@@ -36,20 +39,26 @@ class Answer:
         self.centroids = []
         self.assignment = []
 
-def get_answer(count_centroids, count_points):
+def get_answer(count_centroids, count_points, answer_input):
     answer = Answer()
-    with open('output.txt', 'r') as inp:
-        for _ in range(count_centroids):
-            x, y = list(map(float, inp.readline().split()))
-            answer.centroids.append((x, y))
-        for _ in range(count_points):
-            answer.assignment.append(int(inp.readline()))
+    inp = answer_input
+    for _ in range(count_centroids):
+        x, y = list(map(float, inp.readline().split()))
+        answer.centroids.append((x, y))
+    for _ in range(count_points):
+        answer.assignment.append(int(inp.readline()))
     return answer
 
 class RunResult:
     def __init__(self, exit_code: int, delta: float):
         self.exit_code = exit_code
         self.delta = delta
+
+class FullTestRunResult:
+    def __init__(self, run_result: RunResult, output_file_name: str, output_image_file_name: str):
+        self.run_result: RunResult = run_result
+        self.output_file_name: str = output_file_name
+        self.output_image_file_name: str = output_image_file_name
 
 def run_solution(inputFile, outputFile):
     start = time.time()
@@ -58,12 +67,13 @@ def run_solution(inputFile, outputFile):
     delta = time.time() - start
     return RunResult(process.returncode, delta)
 
-def main():
+def run_test_with_threads(test: str, omp_threads=None):
     use_omp = False
-    omp_threads = None
-    if len(sys.argv) > 1:
+    if omp_threads is not None:
         use_omp = True
-        omp_threads = int(sys.argv[1])
+
+    output_file_name = 'output.txt'
+    output_image_file_name = 'output_image.png'
 
     cmake_generator = CMakeGenerator()
     compilcation_result = compile_solution(cmake_generator, use_omp, omp_threads)
@@ -71,12 +81,10 @@ def main():
     assert(compilcation_result)
 
     # inputFile = input("Enter input file path: ")
-    inputFile = '../lab_2/data/data5.in'
+    inputFile = '../lab_2/data/data' + test + '.in'
     
-    run_result = run_solution(open(inputFile, 'r'), open('output.txt', 'w'))
+    run_result = run_solution(open(inputFile, 'r'), open(output_file_name, 'w'))
     assert(run_result.exit_code == 0)
-
-    print('Run in:', run_result.delta, 'seconds')
 
     with open(inputFile, 'r') as inp:
         n, k = list(map(int, inp.readline().split()))
@@ -85,7 +93,7 @@ def main():
             x, y = list(map(float, inp.readline().split()))
             points.append((x, y))
         colors = generate_colors()[:k]
-        answer = get_answer(k, n)
+        answer = get_answer(k, n, open(output_file_name, 'r'))
 
         plt.scatter(
             list(map(lambda x: x[0], points)),
@@ -99,7 +107,39 @@ def main():
             list(map(lambda x: x[1], answer.centroids)),
             c=colors,
             marker='1')
-        plt.savefig('output_image.png', transparent=True, dpi=600)
+        plt.savefig(output_image_file_name, transparent=True, dpi=600)
+        plt.close()
+    return FullTestRunResult(run_result, output_file_name, output_image_file_name)
+
+def main():
+    tests = ['1', '2', '3', '4', '5']
+    omp_params = [None, 1, 2]
+    descriptions = ['No omp threads', '1 OMP thread', '2 OMP threads']
+    os.makedirs('outputs/outs', exist_ok=True)
+    os.makedirs('outputs/images', exist_ok=True)
+
+    metrics_for_tests = dict()
+
+    for test in tests:
+        metrics_for_tests[test] = []
+        for (omp_param, description) in zip(omp_params, descriptions):
+            result = run_test_with_threads(test, omp_threads=omp_param)
+            shutil.copyfile(result.output_file_name, 'outputs/outs/result' + test + '.out')
+            shutil.copyfile(result.output_image_file_name, 'outputs/images/image_result' + test + '.png')
+            metrics_for_tests[test].append({'description': description, 'time': result.run_result.delta})
+    
+    with open('metrics.json', 'w') as out:
+        out.write(json.dumps(metrics_for_tests))
+
+    for test in metrics_for_tests:
+        indices = [i for i, _ in enumerate(metrics_for_tests[test])]
+        plt.figure(figsize=(10, 4))
+        plt.bar(indices, list(map(lambda x: x['time'], metrics_for_tests[test])))
+        plt.xticks(indices, descriptions)
+        plt.ylabel('Time in seconds')
+        plt.xlabel('Run description')
+        plt.savefig('chart_for_test_' + test + '.png')
+        plt.close()
 
 if __name__ == '__main__':
     main()
