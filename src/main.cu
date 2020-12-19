265771f0bf9b54d8176cc9a7e20ffa85763af600
NevMem <memlolkek@gmail.com>
2020-12-19 14:35:37 +0300

    Checkpoint with some cuda

--- src/main.cu
+++ src/main.cu
@@ -1,41 +1,132 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <cassert>
 
+template <typename T>
+class TypeArrayWrapper {
+public:
+    TypeArrayWrapper(const std::vector<T>& data)
+    {
+        data_ = new T[data.size()];
+        size_ = data.size();
+        for (size_t i = 0; i != size_; ++i) {
+            data_[i] = data[i];
+        }
+    }
+
+    TypeArrayWrapper(size_t size, T fill)
+    {
+        size_ = size;
+        data_ = new T[size];
+        for (size_t i = 0; i != size; ++i) {
+            data_[i] = fill;
+        }
+    }
+
+    bool isOnDevice() const
+    {
+        return onDevice_;
+    }
+
+    T* deviceData() const
+    {
+        assert(onDevice_);
+        return deviceData_;
+    }
+
+    void toDevice()
+    {
+        assert(!onDevice_);
+        initDeviceDataIfNeeded();
+        cudaMemcpy(deviceData_, data_, size_ * sizeof(T), cudaMemcpyHostToDevice);
+        onDevice_ = true;
+    }
+
+    void toHost()
+    {
+        assert(onDevice_);
+        assert(deviceData_);
+        cudaMemcpy(data_, deviceData_, size_ * sizeof(T), cudaMemcpyDeviceToHost);
+        onDevice_ = false;
+    }
+
+    size_t size() const
+    {
+        return size_;
+    }
+
+    T& operator[](size_t index)
+    {
+        assert(!onDevice_);
+        return data_[index];
+    }
+
+    void clear()
+    {
+        clearAll();
+    }
+
+    ~TypeArrayWrapper()
+    {
+        clearAll();
+    }
+
+private:
+    void clearAll()
+    {
+        if (data_) {
+            free(data_);
+            data_ = nullptr;
+        }
+        if (deviceData_) {
+            cudaFree(deviceData_);
+            deviceData_ = nullptr;
+        }
+    }
+
+    void initDeviceDataIfNeeded()
+    {
+        if (!deviceData_) {
+            cudaMalloc(&deviceData_, size_ * sizeof(T));
+        }
+    }
+
+    T* data_ = nullptr;
+    T* deviceData_ = nullptr;
+    size_t size_ = 0;
+
+    bool onDevice_ = false;
+};
 
 __global__ void saxpy(int n, float a, float *x, float *y)
 {
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    if (i < n) y[i] = a*x[i] + y[i];
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) y[i] = a * x[i] + y[i];
 }
 
 int main(void)
 {
-    int N = 1<<20;
-    float *x, *y, *d_x, *d_y;
-    x = (float*)malloc(N*sizeof(float));
-    y = (float*)malloc(N*sizeof(float));
-
-    cudaMalloc(&d_x, N*sizeof(float)); 
-    cudaMalloc(&d_y, N*sizeof(float));
+    int N = 1 << 20;
+    TypeArrayWrapper<float> x(N, 0), y(N, 0);
 
     for (int i = 0; i < N; i++) {
         x[i] = 1.0f;
         y[i] = 2.0f;
     }
 
-    cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+    x.toDevice();
+    y.toDevice();
 
     // Perform SAXPY on 1M elements
-    saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
+    saxpy<<<(N+255)/256, 256>>>(N, 2.0f, x.deviceData(), y.deviceData());
 
-    cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+    /*cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);*/
+    y.toHost();
 
     float maxError = 0.0f;
-    for (int i = 0; i < N; i++)
-        maxError = max(maxError, abs(y[i]-4.0f));
+    for (int i = 0; i < N; i++) {
+        maxError = max(maxError, abs(y[i] - 4.0f));
+    }
     printf("Max error: %f\n", maxError);
-
-    cudaFree(d_x);
-    cudaFree(d_y);
-    free(x);
-    free(y);
 }
