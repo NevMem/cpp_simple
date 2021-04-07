3284da5b9e913db8b86c9bb19b71abd5caf7278e
NevMem <memlolkek@gmail.com>
2021-04-07 22:51:38 +0300

    Smth

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,6 @@
 #include <set>
 #include <future>
 
-std::shared_ptr< std::vector<int> > smth()
-{
-    return std::make_shared<std::vector<int> >(
-        std::initializer_list<int> { 1, 2, 3, 4 }
-    );
-}
-
 int main() {
-    std::cout << "here" << std::endl;
-    const auto vec = *smth();
-    for (const int& elem: vec) {
-        std::cout << "i'm here" << std::endl;
-        std::cout << elem << std::endl;
-    }
-    std::cout << "after" << std::endl;
+    
 }
